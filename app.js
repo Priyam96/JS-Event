54ef7fae0a9251b4bb9cb9d2346b695832b4c5de
Priyam Jha <pkj995@gmail.com>
2023-05-11 03:56:44 +0530

    Event v1

---
+++ app.js
@@ -0,0 +1,14 @@
+//JS Events
+
+// function test(){
+//     console.log('clicked on the card');
+// }
+let alertDiv = document.getElementById('alert2');
+function mouseover(){
+    alertDiv.style.display = 'block'
+}
+
+function mouseout(){
+    alertDiv.style.display = 'none'
+}
+
